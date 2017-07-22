75e84baa486c925f47aad414565aee23d5c88bc8
ljvmiranda921 <lester.miranda@obf.ateneo.edu>
2017-07-22 17:42:20 +0900

    Add rosenbrock and ackley (#6)
    The following single-objective test functions were
    implemented in pyswarms/utils/functions/single_obj.py
      - rosenbrock_func()
      - ackley_func()
    
    In addition, unit tests accompanying these implementations
    were created.
    
    Author: ljvmiranda921

--- tests/test_pyswarms.py
+++ tests/test_pyswarms.py
@@ -15,17 +15,26 @@ class TestSingleObj(unittest.TestCase):
 
     def setUp(self):
         """Set up test fixtures."""
-        # Test swarm
-        self.x = np.array([[0,0],[0,0],[0,0]])
+        # Test swarm with size=3, dimensions=2
+        self.input = np.zeros([3,2])
+        # Target value
+        self.target = np.zeros(3)
 
     def test_sphere(self):
         """Test sphere function."""
-        assert fx.sphere_func(self.x).all() == np.array([0,0,0]).all()
+        assert fx.sphere_func(self.input).all() == self.target.all()
 
     def test_rastrigin(self):
         """Test rastrigin function."""
-        assert fx.rastrigin_func(self.x).all() == np.array([0,0,0]).all()
+        assert fx.rastrigin_func(self.input).all() == self.target.all()
 
+    def test_ackley(self):
+        """Test ackley function."""
+        assert np.isclose(fx.ackley_func(self.input), self.target).all()
+
+    def test_rosenbrock(self):
+        """Test rosenbrock function."""
+        assert fx.rosenbrock_func(np.ones([3,2])).all() == np.array([0,0,0]).all()
 
 if __name__ == '__main__':
     unittest.main()
