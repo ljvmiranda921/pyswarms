d1363701d17b16ba9ab6c381d31f600fa9ff1e74
ljvmiranda921 <lester.miranda@obf.ateneo.edu>
2017-07-22 16:45:01 +0900

    Supports Python 3.4 and 3.5

--- setup.py
+++ setup.py
@@ -45,7 +45,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
