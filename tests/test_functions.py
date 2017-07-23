6394b2ae5fa6d96845634fb2794f28c08f215987
ljvmiranda921 <lester.miranda@obf.ateneo.edu>
2017-07-23 17:26:58 +0900

    Implement unit tests for global-best PSO
    From this point forward, we'll be implementing a test-driven
    development scheme. This is also helpful so that we can be
    strict with ourselves come integration time with TravisCI.
    
    For the global best PSO, various checks on edge cases are
    currently implemented. Most of these are implemented using
    the unittest module. Check `test_gb.py` as a guide.
    
    Author: ljvmiranda921

---
+++ tests/test_functions.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `pyswarms` package."""
+
+# Import modules
+import unittest
+import numpy as np
+
+# Import from package
+from pyswarms.utils.functions import single_obj as fx
+
+
+class TestSingleObj(unittest.TestCase):
+    """Tests all single-objective functions."""
+
+    def setUp(self):
+        """Set up test fixtures."""
+        # Test swarm with size=3, dimensions=2
+        self.input = np.zeros([3,2])
+        # Target value
+        self.target = np.zeros(3)
+
+    def test_sphere(self):
+        """Tests sphere function."""
+        self.assertEqual(fx.sphere_func(self.input).all(), self.target.all())
+
+    def test_rastrigin(self):
+        """Tests rastrigin function."""
+        self.assertEqual(fx.rastrigin_func(self.input).all(), self.target.all())
+
+    def test_ackley(self):
+        """Tests ackley function."""
+        assert np.isclose(fx.ackley_func(self.input), self.target).all()
+
+    def test_rosenbrock(self):
+        """Tests rosenbrock function."""
+        self.assertEqual(fx.rosenbrock_func(np.ones([3,2])).all(), np.array([0,0,0]).all())
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_pyswarms.py
+++ tests/test_pyswarms.py
@@ -9,32 +9,63 @@ import numpy as np
 
 # Import from package
 from pyswarms.utils.functions import single_obj as fx
+from pyswarms.single import GBestPSO
 
-class TestSingleObj(unittest.TestCase):
-    """Tests all single-objective functions."""
+class TestGlobalBest(unittest.TestCase):
+    """Class for testing pyswarms.single.GBestPSO"""
 
     def setUp(self):
-        """Set up test fixtures."""
-        # Test swarm with size=3, dimensions=2
-        self.input = np.zeros([3,2])
-        # Target value
-        self.target = np.zeros(3)
+        """Set up test fixtures """
+        self.options = {'c1':0.5, 'c2':0.7, 'm':0.5}
+        self.safe_bounds = (np.array([-5,-5]), np.array([5,5]))
 
-    def test_sphere(self):
-        """Test sphere function."""
-        assert fx.sphere_func(self.input).all() == self.target.all()
+    def test_keyword_check_fail(self):
+        """Tests if exceptions are thrown when keywords are missing"""
+        check_c1 = {'c2':0.7, 'm':0.5}
+        check_c2 = {'c1':0.5, 'm':0.5}
+        check_m = {'c1':0.5, 'c2':0.7}
+        with self.assertRaises(AssertionError):
+            optimizer = GBestPSO(5,2,**check_c1)
+            optimizer = GBestPSO(5,2,**check_c2)
+            optimizer = GBestPSO(5,2,**check_m)
 
-    def test_rastrigin(self):
-        """Test rastrigin function."""
-        assert fx.rastrigin_func(self.input).all() == self.target.all()
+    def test_bound_size_fail(self):
+        """Tests if exception is thrown when bound length is not 2"""
+        bounds = (np.array([-5,-5]))
+        with self.assertRaises(AssertionError):
+            optimizer = GBestPSO(5,2, bounds, **self.options)
 
-    def test_ackley(self):
-        """Test ackley function."""
-        assert np.isclose(fx.ackley_func(self.input), self.target).all()
+    def test_bound_type_fail(self):
+        """Tests if exception is thrown when bound type is not tuple"""
+        bounds = [np.array([-5,-5]), np.array([5,5])]
+        with self.assertRaises(AssertionError):
+            optimizer = GBestPSO(5,2, bounds, **self.options)
 
-    def test_rosenbrock(self):
-        """Test rosenbrock function."""
-        assert fx.rosenbrock_func(np.ones([3,2])).all() == np.array([0,0,0]).all()
+    def test_bound_maxmin_fail(self):
+        """Tests if exception is thrown when min max of the bound is
+        wrong.
+        """
+        bounds_1 = (np.array([5,5]), np.array([-5,-5]))
+        bounds_2 = (np.array([5,-5]), np.array([-5,5]))
+        with self.assertRaises(AssertionError):
+            optimizer = GBestPSO(5,2, bounds_1, **self.options)
+            optimizer = GBestPSO(5,2, bounds_2, **self.options)
+
+    def test_bound_shapes_fail(self):
+        """Tests if exception is thrown when bounds are of unequal 
+        shapes.
+        """
+        bounds = (np.array([-5,-5,-5]), np.array([5,5]))
+        with self.assertRaises(AssertionError):
+            optimizer = GBestPSO(5,2, bounds, **self.options)
+
+    def test_bound_shape_dims_fail(self):
+        """Test if exception is thrown when bound shape is not equal
+        to dims."""
+        bounds = (np.array([-5,-5,-5]), np.array([5,5,5]))
+        with self.assertRaises(AssertionError):
+            optimizer = GBestPSO(5,2, bounds, **self.options)
+
 
 if __name__ == '__main__':
     unittest.main()
