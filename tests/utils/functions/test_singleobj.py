572b3cd0f6a67c88f9b71ed2a123123b8465cf40
ljvmiranda921 <lester.miranda@obf.ateneo.edu>
2017-07-26 08:04:48 +0900

    Additional tests for skeleton functions

--- tests/utils/functions/test_singleobj.py
+++ tests/utils/functions/test_singleobj.py
@@ -18,10 +18,74 @@ class TestSingleObj(unittest.TestCase):
         """Set up test fixtures."""
         # Test swarm with size=3, dimensions=2
         self.input = np.zeros([3,2])
+        self.input2 = np.ones([3,2])
+        # Erroneous input - dimension
+        self.bad_input = np.zeros([3,3])
         # Target value
         self.target = np.zeros(3)
         # Target output size
         self.target_size = (self.input.shape[0], )
+
+class InputBoundFail(TestSingleObj):
+    """Tests exception throws when fed with erroneous input."""
+
+    def test_rastrigin_bound_fail(self):
+        """Test rastrigin bound exception"""
+        x = - np.random.randint(low=6,high=100,size=(3,2))
+        x_ = np.random.randint(low=6,high=100,size=(3,2))
+        with self.assertRaises(AssertionError):
+            fx.rastrigin_func(x)
+            fx.rastrigin_func(x_)
+
+    def test_ackley_bound_fail(self):
+        """Test ackley bound exception"""
+        x = - np.random.randint(low=32,high=100,size=(3,2))
+        x_ = np.random.randint(low=32,high=100,size=(3,2))
+        with self.assertRaises(AssertionError):
+            fx.ackley_func(x)
+            fx.ackley_func(x_)
+
+    def test_beale_bound_fail(self):
+        """Test beale bound exception"""
+        x = - np.random.randint(low=4.6666,high=100,size=(3,2))
+        x_ = np.random.randint(low=4.6666,high=100,size=(3,2))
+        with self.assertRaises(AssertionError):
+            fx.beale_func(x)
+            fx.beale_func(x_)
+
+    def test_goldstein_bound_fail(self):
+        """Test goldstein bound exception"""
+        x = - np.random.randint(low=2.00001,high=100,size=(3,2))
+        x_ = np.random.randint(low=2.00001,high=100,size=(3,2))
+        with self.assertRaises(AssertionError):
+            fx.goldstein_func(x)
+            fx.goldstein_func(x_)
+
+    def test_booth_bound_fail(self):
+        """Test booth bound exception"""
+        x = - np.random.randint(low=11.00001,high=100,size=(3,2))
+        x_ = np.random.randint(low=11.00001,high=100,size=(3,2))
+        with self.assertRaises(AssertionError):
+            fx.booth_func(x)
+            fx.booth_func(x_)
+
+class InputDimFail(TestSingleObj):
+    """Tests exception throws when fed with erroneous dimension."""
+
+    def test_beale_dim_fail(self):
+        """Test beale dim exception"""
+        with self.assertRaises(AssertionError):
+            fx.beale_func(self.bad_input)
+
+    def test_goldstein_dim_fail(self):
+        """Test golstein dim exception"""
+        with self.assertRaises(AssertionError):
+            fx.goldstein_func(self.bad_input)
+
+    def test_booth_dim_fail(self):
+        """Test booth dim exception"""
+        with self.assertRaises(AssertionError):
+            fx.booth_func(self.bad_input)
 
 class ExpectedOutput(TestSingleObj):
     """Tests if a function outputs a minima if fed with expected argmin."""
@@ -40,7 +104,22 @@ class ExpectedOutput(TestSingleObj):
 
     def test_rosenbrock_output(self):
         """Tests rosenbrock function output."""
-        self.assertEqual(fx.rosenbrock_func(np.ones([3,2])).all(),np.zeros(3).all())
+        self.assertEqual(fx.rosenbrock_func(self.input2).all(),np.zeros(3).all())
+
+    def test_beale_output(self):
+        """Tests beale function output."""
+        assert np.isclose(fx.beale_func([3, 0.5] * self.input2),
+            self.target).all()
+
+    def test_goldstein_output(self):
+        """Tests goldstein-price function output."""
+        assert np.isclose(fx.goldstein_func([0, -1] * self.input2),
+            (3 * np.ones(3))).all()
+
+    def test_booth_output(self):
+        """Test booth function output."""
+        assert np.isclose(fx.booth_func([1, 3] * self.input2),
+            self.target).all()
 
 class OutputSize(TestSingleObj):
     """Tests if the output of the function is the same as no. of particles"""
@@ -61,24 +140,17 @@ class OutputSize(TestSingleObj):
         """Tests rosenbrock output size."""
         self.assertEqual(fx.rosenbrock_func(self.input).shape, self.target_size)
 
-class BoundFail(TestSingleObj):
-    """Tests exception throws when fed with erroneous input."""
+    def test_beale_output_size(self):
+        """Tests beale output size."""
+        self.assertEqual(fx.beale_func(self.input).shape, self.target_size)
 
-    def test_rastrigin_bound_fail(self):
-        """Test rastrigin bound exception"""
-        x = - np.random.randint(low=6,high=100,size=(3,2))
-        x_ = np.random.randint(low=6,high=100,size=(3,2))
-        with self.assertRaises(AssertionError):
-            fx.rastrigin_func(x)
-            fx.rastrigin_func(x_)
+    def test_goldstein_output_size(self):
+        """Test goldstein output size."""
+        self.assertEqual(fx.goldstein_func(self.input).shape, self.target_size)
 
-    def test_ackley_bound_fail(self):
-        """Test ackley bound exception"""
-        x = - np.random.randint(low=32,high=100,size=(3,2))
-        x_ = np.random.randint(low=32,high=100,size=(3,2))
-        with self.assertRaises(AssertionError):
-            fx.ackley_func(x)
-            fx.ackley_func(x_)
+    def test_booth_output_size(self):
+        """Test booth output size."""
+        self.assertEqual(fx.booth_func(self.input).shape, self.target_size)
 
 if __name__ == '__main__':
     unittest.main()
