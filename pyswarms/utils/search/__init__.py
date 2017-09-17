640e44ced6377603b3f50dfdb539563946cf91c1
ljvmiranda921 <lester.miranda@obf.ateneo.edu>
2017-09-17 17:11:22 +0900

    Add direct import to search tools
    This commit updates the __init__.py file to include the two
    search methods when importing the pyswarms.utils.search
    module. In this case, we don't have to include grid_search
    and random_search explicitly.
    
    In addition, I removed the comments in the environments
    initialization file to avoid confusion as to where to put
    the search methods.
    
    Author: ljvmiranda921
    Email: ljvmiranda921@gmail.com

--- pyswarms/utils/search/__init__.py
+++ pyswarms/utils/search/__init__.py
@@ -3,3 +3,10 @@ The :mod:`pyswarms.utils.search` module implements various techniques in
 hyperparameter value optimization.
 """
 
+from .grid_search import GridSearch
+from .random_search import RandomSearch
+
+__all__ = [
+    "GridSearch",
+    "RandomSearch"
+    ]
