2b2875488e8501453ef47f5beb61823f4765d34d
mamady <manabinto@gmail.com>
2018-04-06 00:07:18 +0200

    Update test

--- tests/optimizers/test_local_best.py
+++ tests/optimizers/test_local_best.py
@@ -204,10 +204,10 @@ class RunOptimize(Base):
         """Check if setting ftol breaks the optimization process
         accordingly."""
         # Perform a simple optimization
-        optimizer = LocalBestPSO(5,2, options=self.options, ftol=1e-1)
-        optimizer.optimize(sphere_func, 5000, verbose=0)
+        optimizer = LocalBestPSO(10,2, options=self.options, ftol=1e-1)
+        optimizer.optimize(sphere_func, 2000, verbose=0)
         cost_hist = optimizer.get_cost_history
-        self.assertNotEqual(cost_hist.shape, (5000, ))
+        self.assertNotEqual(cost_hist.shape, (2000, ))
 
 if __name__ == '__main__':
     unittest.main()
