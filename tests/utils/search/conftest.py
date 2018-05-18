a9e33a14e71ab9c113d9c365a5d2111eaceedabd
ljvmiranda921 <lester.miranda@obf.ateneo.edu>
2018-05-18 16:35:47 +0900

    Update tests in RandomSearch

--- tests/utils/search/conftest.py
+++ tests/utils/search/conftest.py
@@ -9,6 +9,7 @@ import numpy as np
 
 # Import from package
 from pyswarms.utils.search.grid_search import GridSearch
+from pyswarms.utils.search.random_search import RandomSearch
 from pyswarms.single import LocalBestPSO
 from pyswarms.utils.functions.single_obj import sphere_func
 
@@ -25,7 +26,7 @@ def grid():
 
 @pytest.fixture
 def grid_mini():
-    """Returns a Gridsearch instance with a smaller search-space"""
+    """Returns a GridSearch instance with a smaller search-space"""
     options = {'c1': [1, 2],
                'c2': 6,
                'k' : 5,
@@ -33,3 +34,24 @@ def grid_mini():
                'p' : 0}
     return GridSearch(LocalBestPSO, n_particles=40,
         dimensions=20, options=options, objective_func=sphere_func, iters=10, bounds=None, velocity_clamp=None)
+
+@pytest.fixture
+def random_unbounded():
+    """Returns a RandomSearch instance without bounds"""
+    options = {'c1': [1, 5],
+               'c2': [6, 10],
+               'k' : [11, 15],
+               'w' : [0.4, 0.9],
+               'p' : 1}
+    return RandomSearch(LocalBestPSO, n_particles=40, dimensions=20, options=options, objective_func=sphere_func, iters=10, n_selection_iters=100, bounds=None, velocity_clamp=None)
+
+@pytest.fixture
+def random_bounded():
+    """Returns a RandomSearch instance with bounds"""
+    bounds = (np.array([-5,-5]), np.array([5,5]))
+    options = {'c1': [1, 5],
+               'c2': [6, 10],
+               'k' : [11, 15],
+               'w' : [0.4, 0.9],
+               'p' : 1}
+    return RandomSearch(LocalBestPSO, n_particles=40, dimensions=20, options=options, objective_func=sphere_func, iters=10, n_selection_iters=100, bounds=bounds, velocity_clamp=None)
