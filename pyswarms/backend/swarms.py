68c37d4d534816505e9cacec28463bded1b4699a
ljvmiranda921 <lester.miranda@obf.ateneo.edu>
2018-05-20 12:46:32 +0900

    Add Swarm class in backend
    This class creates a DataClass-like interface for building swarms.
    It holds all special attributes in swarm optimization that you can
    reuse in case the user wants to build their own evolutionary loop.
    
    Signed-off-by: Lester James V. Miranda <ljvmiranda@gmail.com>

---
+++ pyswarms/backend/swarms.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+"""
+Swarm Class Backend
+
+This module implements a Swarm class that holds various attributes in
+the swarm such as position, velocity, behavior, etc. You can use this
+as input to most backend cases
+"""
+
+# Import modules
+import numpy as np
+from attr import (attrs, attrib)
+from attr.validators import instance_of
+
+@attrs
+class Swarm(object):
+    """A Swarm Class
+    
+    This class offers a generic swarm that can be used in most use-cases
+    such as single-objective optimization, etc. It contains various attributes
+    that are commonly-used in most swarm implementations.
+    """
+    # Required attributes
+    position = attrib(type=np.ndarray, validator=instance_of(np.ndarray))
+    velocity = attrib(type=np.ndarray, validator=instance_of(np.ndarray))
+    # With defaults
+    n_particles = attrib(type=int, validator=instance_of(int))
+    dimensions = attrib(type=int, validator=instance_of(int))
+    behavior = attrib(type=dict, default={}, validator=instance_of(dict))
+    pbest_pos = attrib(type=np.ndarray, validator=instance_of(np.ndarray))
+    best_pos  = attrib(type=np.ndarray, default=np.array([]), validator=instance_of(np.ndarray))
+    pbest_cost = attrib(type=np.ndarray, default=np.array([]), validator=instance_of(np.ndarray))
+    best_cost = attrib(type=float, default=np.inf, validator=instance_of((int, float)))
+    current_cost = attrib(type=np.ndarray, default=np.array([]), validator=instance_of(np.ndarray))
+
+    @n_particles.default
+    def n_particles_default(self):
+        return self.position.shape[0]
+
+    @dimensions.default
+    def dimensions_default(self):
+        return self.position.shape[1]
+
+    @pbest_pos.default
+    def pbest_pos_default(self):
+        return self.position
