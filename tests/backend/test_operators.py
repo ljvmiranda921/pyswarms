215909c0d140ed8b3d49b1aed96ca9343cd9898d
ljvmiranda921 <lester.miranda@obf.ateneo.edu>
2018-05-24 20:06:17 +0900

    Move functions to topologies
    In this commit, we move the global best computation to the Topology
    classes themselves, but keep the compute_pbest(), compute_position() and
    compute_velocity() so that people can reuse it.  The position and
    velocity computations are standard implementations, so this is just
    imported by the Ring and Star topology classes in their own methods
    
    Signed-off-by: Lester James V. Miranda <ljvmiranda@gmail.com>

--- tests/backend/test_operators.py
+++ tests/backend/test_operators.py
@@ -17,14 +17,6 @@ def test_update_pbest_return_values(swarm):
     assert (pos == expected_pos).all()
     assert (cost == expected_cost).all()
 
-def test_update_gbest_return_values(swarm):
-    """Test if update_gbest() gives the expected return values"""
-    expected_cost = 1
-    expected_pos = np.array([1,2,3])
-    pos, cost = P.update_gbest(swarm)
-    assert cost == expected_cost
-    assert (pos == expected_pos).all()
-
 @pytest.mark.parametrize('clamp', [None, (0,1), (-1,1)])
 def test_update_velocity_return_values(swarm, clamp):
     """Test if update_velocity() gives the expected shape and range"""
@@ -41,14 +33,3 @@ def test_update_position_return_values(swarm, bounds):
     assert p.shape == swarm.velocity.shape
     if bounds is not None:
         assert (bounds[0] <= p).all() and (bounds[1] >= p).all()
-
-@pytest.mark.parametrize('k', [1,2,3])
-@pytest.mark.parametrize('p', [1,2])
-def test_update_gbest_neighborhood(swarm, p, k):
-    """Test if update_gbest_neighborhood gives the expected return values"""
-    pos, cost = P.update_gbest_neighborhood(swarm, p=p, k=k)
-    expected_pos = np.array([1,2,3])
-    expected_cost = 1
-    print('k={} p={}, pos={} cost={}'.format(k,p,pos,cost))
-    assert (pos == expected_pos).all()
-    assert cost == expected_cost
