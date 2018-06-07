d8b68dbf204cfc543ed3d3d0e8d77dc413651fdd
ljvmiranda921 <lester.miranda@obf.ateneo.edu>
2018-06-07 11:15:09 +0900

    Add tests for init_pos
    Reference: #119
    
    Tests the init_pos feature for swarm generation. Checks
    cases when:
      - init_pos and bounds is not None
      - init_pos and bounds is None
      - either init_pos or bounds is None (2 cases)
    
    Signed-off-by: Lester James V. Miranda <ljvmiranda@gmail.com>

--- tests/backend/test_generators.py
+++ tests/backend/test_generators.py
@@ -23,6 +23,32 @@ def test_generate_swarm_return_values(bounds, center):
     upper_bound = center * np.array(max_bounds)
     assert (pos <= upper_bound).all() and (pos >= lower_bound).all()
 
+
+@pytest.mark.parametrize('bounds', [None, ([1,1,1], [10,10,10])])
+@pytest.mark.parametrize('init_pos', [None, np.array([[2,5,6],[7,2,1]])])
+def test_generate_swarm_bounds_init_pos(bounds, init_pos):
+    """Tests if generate_swarm() returns expected values given init_pos and bounds"""
+    pos = P.generate_swarm(n_particles=2, dimensions=3, bounds=bounds,
+                           init_pos=init_pos)
+    if (bounds is None) and (init_pos is None):
+        min_bounds, max_bounds = (0.0, 1.00)
+    elif (bounds is None) and (init_pos is not None):
+        min_bounds, max_bounds = (-np.inf, np.inf)
+    else:
+        min_bounds, max_bounds = bounds
+    lower_bound = np.array(min_bounds)
+    upper_bound = np.array(max_bounds)
+    assert (pos <= upper_bound).all() and (pos >= lower_bound).all()
+
+def test_generate_swarm_out_of_bounds():
+    """Tests if generate_swarm() raises ValueError when initialized with the wrong value"""
+    bounds = ([1,1,1], [5,5,5])
+    init_pos = np.array([[-2,3,3], [6,8,1]])
+    with pytest.raises(ValueError):
+        pos = P.generate_swarm(n_particles=2, dimensions=3, bounds=bounds,
+                               init_pos=init_pos)
+
+
 @pytest.mark.parametrize('clamp', [None, (0,1), (2,5), (1,6)])
 def test_generate_velocity_return_values(clamp):
     """Tests if generate_velocity() returns expected values"""
