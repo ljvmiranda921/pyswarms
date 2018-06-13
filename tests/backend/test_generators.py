054570e9ef3ffb753824629123afa3f4d963d55e
ljvmiranda921 <lester.miranda@obf.ateneo.edu>
2018-06-13 21:46:46 +0900

    Fix bad behavior in discrete swarm generation
    Sometimes your swarm can generate a pure 0 or pure 1 vector,
    we should account that.
    
    Signed-off-by: Lester James V. Miranda <ljvmiranda@gmail.com>

--- tests/backend/test_generators.py
+++ tests/backend/test_generators.py
@@ -23,6 +23,34 @@ def test_generate_swarm_return_values(bounds, init_pos):
     upper_bound = init_pos * np.array(max_bounds)
     assert (pos <= upper_bound).all() and (pos >= lower_bound).all()
 
+def test_generate_swarm_out_of_bounds():
+    """Tests if generate_swarm() raises ValueError when initialized with the wrong value"""
+    bounds = ([1,1,1], [5,5,5])
+    init_pos = np.array([[-2,3,3], [6,8,1]])
+    with pytest.raises(ValueError):
+        pos = P.generate_swarm(n_particles=2, dimensions=3, bounds=bounds,
+                               init_pos=init_pos)
+
+@pytest.mark.parametrize('binary', [False, True])
+def test_generate_discrete_binary_swarm(binary):
+    """Tests if generate_discrete_swarm(binary=True) returns expected values"""
+    dims = 3
+    pos = P.generate_discrete_swarm(n_particles=2, dimensions=dims, binary=binary)
+    if binary:
+        assert len(np.unique(pos)) <= 2 # Might generate pure 0 or 1
+    else:
+        assert (np.max(pos, axis=1) == dims - 1).all()
+
+@pytest.mark.parametrize('init_pos', [None, np.array([[4,2,1], [1,4,6]])])
+def test_generate_discrete_swarm(init_pos):
+    """Tests if init_pos actually sets the position properly"""
+    dims = 3
+    pos = P.generate_discrete_swarm(n_particles=2, dimensions=dims, init_pos=init_pos)
+    if init_pos is None:
+        assert (np.max(pos, axis=1) == dims - 1).all()
+    else:
+        assert np.equal(pos, init_pos).all()
+
 @pytest.mark.parametrize('clamp', [None, (0,1), (2,5), (1,6)])
 def test_generate_velocity_return_values(clamp):
     """Tests if generate_velocity() returns expected values"""
