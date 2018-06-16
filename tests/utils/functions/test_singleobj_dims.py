e54768dd7a0a6bd8f0ef65cc060957a42e54114b
ljvmiranda921 <lester.miranda@obf.ateneo.edu>
2018-06-17 00:18:31 +0900

    Format tests code to black

--- tests/utils/functions/test_singleobj_dims.py
+++ tests/utils/functions/test_singleobj_dims.py
@@ -11,35 +11,42 @@ from collections import namedtuple
 # Import from package
 from pyswarms.utils.functions import single_obj as fx
 
+
 def test_beale_dim_fail(outdim):
     """Test beale dim exception"""
     with pytest.raises(IndexError):
         fx.beale_func(outdim)
+
 
 def test_goldstein_dim_fail(outdim):
     """Test goldstein dim exception"""
     with pytest.raises(IndexError):
         fx.goldstein_func(outdim)
 
+
 def test_booth_dim_fail(outdim):
     """Test booth dim exception"""
     with pytest.raises(IndexError):
         fx.booth_func(outdim)
+
 
 def test_bukin6_dim_fail(outdim):
     """Test bukin6 dim exception"""
     with pytest.raises(IndexError):
         fx.bukin6_func(outdim)
 
+
 def test_matyas_dim_fail(outdim):
     """Test matyas dim exception"""
     with pytest.raises(IndexError):
         fx.matyas_func(outdim)
 
+
 def test_levi_dim_fail(outdim):
     """Test levi dim exception"""
     with pytest.raises(IndexError):
         fx.levi_func(outdim)
+
 
 def test_schaffer2_dim_fail(outdim):
     """Test schaffer2 dim exception"""
--- tests/utils/plotters/conftest.py
+++ tests/utils/plotters/conftest.py
@@ -10,26 +10,29 @@ import numpy as np
 from mock import Mock
 import matplotlib as mpl
 
-if os.environ.get('DISPLAY','') == '':
-    mpl.use('Agg')
+if os.environ.get("DISPLAY", "") == "":
+    mpl.use("Agg")
 
 # Import from package
 from pyswarms.single import GlobalBestPSO
 from pyswarms.utils.functions.single_obj import sphere_func
 from pyswarms.utils.plotters.formatters import Mesher
 
+
 @pytest.fixture
 def trained_optimizer():
     """Returns a trained optimizer instance with 100 iterations"""
-    options = {'c1':0.5, 'c2':0.3, 'w':0.9}
+    options = {"c1": 0.5, "c2": 0.3, "w": 0.9}
     optimizer = GlobalBestPSO(n_particles=10, dimensions=2, options=options)
     optimizer.optimize(sphere_func, iters=100)
     return optimizer
+
 
 @pytest.fixture
 def pos_history():
     """Returns a list containing a swarms' position history"""
     return np.random.uniform(size=(10, 5, 2))
+
 
 @pytest.fixture
 def mesher():
