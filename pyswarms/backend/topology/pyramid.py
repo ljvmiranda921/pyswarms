f595feb9eb439eaac62c81152483df337cde697b
whzup <39431903+whzup@users.noreply.github.com>
2018-06-23 13:51:42 +0200

    Fix indentation
    Fix indentation error in the Pyramid class that prevented the tests from running.
    
    See also: #142

--- pyswarms/backend/topology/pyramid.py
+++ pyswarms/backend/topology/pyramid.py
@@ -72,62 +72,62 @@ class Pyramid(Topology):
         else:
             return (best_pos, best_cost)
             
-        def compute_velocity(self, swarm, clamp=None):
-            """Computes the velocity matrix
+    def compute_velocity(self, swarm, clamp=None):
+        """Computes the velocity matrix
 
-            This method updates the velocity matrix using the best and current
-            positions of the swarm. The velocity matrix is computed using the
-            cognitive and social terms of the swarm.
-    
-            A sample usage can be seen with the following:
-    
-            .. code-block :: python
-    
-                import pyswarms.backend as P
-                from pyswarms.swarms.backend import Swarm
-                from pyswarms.backend.topology import Pyramid
-    
-                my_swarm = P.create_swarm(n_particles, dimensions)
-                my_topology = Pyramid()
-    
-                for i in range(iters):
-                    # Inside the for-loop
-                    my_swarm.velocity = my_topology.update_velocity(my_swarm, clamp)
-    
-            Parameters
-            ----------
-            swarm : pyswarms.backend.swarms.Swarm
-                a Swarm instance
-            clamp : tuple of floats (default is :code:`None`)
-                a tuple of size 2 where the first entry is the minimum velocity
-                and the second entry is the maximum velocity. It
-                sets the limits for velocity clamping.
-    
-            Returns
-            -------
-            numpy.ndarray
-                Updated velocity matrix
-            """
-            return ops.compute_velocity(swarm, clamp)
-        
-        def compute_position(self, swarm, bounds=None):
-            """Updates the position matrix
+        This method updates the velocity matrix using the best and current
+        positions of the swarm. The velocity matrix is computed using the
+        cognitive and social terms of the swarm.
 
-            This method updates the position matrix given the current position and
-            the velocity. If bounded, it waives updating the position.
+        A sample usage can be seen with the following:
+
+        .. code-block :: python
+
+            import pyswarms.backend as P
+            from pyswarms.swarms.backend import Swarm
+            from pyswarms.backend.topology import Pyramid
+
+            my_swarm = P.create_swarm(n_particles, dimensions)
+            my_topology = Pyramid()
+
+            for i in range(iters):
+                # Inside the for-loop
+                my_swarm.velocity = my_topology.update_velocity(my_swarm, clamp)
+
+        Parameters
+        ----------
+        swarm : pyswarms.backend.swarms.Swarm
+            a Swarm instance
+        clamp : tuple of floats (default is :code:`None`)
+            a tuple of size 2 where the first entry is the minimum velocity
+            and the second entry is the maximum velocity. It
+            sets the limits for velocity clamping.
+
+        Returns
+        -------
+        numpy.ndarray
+            Updated velocity matrix
+        """
+        return ops.compute_velocity(swarm, clamp)
     
-            Parameters
-            ----------
-            swarm : pyswarms.backend.swarms.Swarm
-                a Swarm instance
-            bounds : tuple of :code:`np.ndarray` or list (default is :code:`None`)
-                a tuple of size 2 where the first entry is the minimum bound while
-                the second entry is the maximum bound. Each array must be of shape
-                :code:`(dimensions,)`.
-    
-            Returns
-            -------
-            numpy.ndarray
-                New position-matrix
-            """
-            return ops.compute_position(swarm, bounds)
+    def compute_position(self, swarm, bounds=None):
+        """Updates the position matrix
+
+        This method updates the position matrix given the current position and
+        the velocity. If bounded, it waives updating the position.
+
+        Parameters
+        ----------
+        swarm : pyswarms.backend.swarms.Swarm
+            a Swarm instance
+        bounds : tuple of :code:`np.ndarray` or list (default is :code:`None`)
+            a tuple of size 2 where the first entry is the minimum bound while
+            the second entry is the maximum bound. Each array must be of shape
+            :code:`(dimensions,)`.
+
+        Returns
+        -------
+        numpy.ndarray
+            New position-matrix
+        """
+        return ops.compute_position(swarm, bounds)
