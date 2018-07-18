4d0e69a8e9cb54eff45c41016d796cd92da30bf8
Jay Speidell <jayspeidell@gmail.com>
2018-07-18 10:04:22 -0700

    Add objective functions (#168)
    Reference: #158 
    
    Verified Goldstein and Booth functions working and removed TODO tags. Fixed Bulkin No 6 and removed TODO tag. Implemented Cross-in-Tray, Easom, Eggholder, Himmelblau's, Holder Table, and Three Hump Camel functions. The file is getting long so I sorted the functions and added a list of functions and their corresponding function name to the docstring.
    Added the new functions in the test files for the objective functions.
    
    Signed-off-by: Aaron (@whzup)
    Commited-with: Jay Speidell (@jayspeidell)

--- tests/utils/functions/test_singleobj_return.py
+++ tests/utils/functions/test_singleobj_return.py
@@ -10,14 +10,7 @@ from collections import namedtuple
 from pyswarms.utils.functions import single_obj as fx
 
 
-def test_sphere_output(common_minima):
-    """Tests sphere function output."""
-    assert np.array_equal(fx.sphere_func(common_minima), np.zeros((3,)))
 
-
-def test_rastrigin_output(common_minima):
-    """Tests rastrigin function output."""
-    assert np.array_equal(fx.rastrigin_func(common_minima), np.zeros(3))
 
 
 def test_ackley_output(common_minima):
@@ -25,24 +18,10 @@ def test_ackley_output(common_minima):
     assert np.isclose(fx.ackley_func(common_minima), np.zeros(3)).all()
 
 
-def test_rosenbrock_output(common_minima2):
-    """Tests rosenbrock function output."""
-    assert np.array_equal(
-        fx.rosenbrock_func(common_minima2).all(), np.zeros(3).all()
-    )
-
-
 def test_beale_output(common_minima2):
     """Tests beale function output."""
     assert np.isclose(
         fx.beale_func([3, 0.5] * common_minima2), np.zeros(3)
-    ).all()
-
-
-def test_goldstein_output(common_minima2):
-    """Tests goldstein-price function output."""
-    assert np.isclose(
-        fx.goldstein_func([0, -1] * common_minima2), (3 * np.ones(3))
     ).all()
 
 
@@ -60,9 +39,85 @@ def test_bukin6_output(common_minima2):
     ).all()
 
 
-def test_bukin6_output(common_minima):
-    """Test bukin function output."""
-    assert np.isclose(fx.matyas_func(common_minima), np.zeros(3)).all()
+@pytest.mark.parametrize(
+    "x",
+    [
+        np.array([[1.34941, -1.34941],
+                  [1.34941, 1.34941],
+                  [-1.34941, 1.34941],
+                  [-1.34941, -1.34941]])
+    ],
+)
+@pytest.mark.parametrize(
+    "minima",
+    [
+        np.array([-2.06261, -2.06261, -2.06261, -2.06261])
+    ],
+)
+def test_crossintray_output(x, minima):
+    """Tests crossintray function output."""
+    assert np.isclose(
+        fx.crossintray_func(x), minima
+    ).all()
+
+
+def test_easom_output(common_minima2):
+    """Tests easom function output."""
+    assert np.isclose(
+        fx.easom_func([np.pi, np.pi] * common_minima2), (-1 * np.ones(3))
+    ).all()
+
+
+def test_eggholder_output(common_minima2):
+    """Tests eggholder function output."""
+    assert np.isclose(
+        fx.eggholder_func([512, 404.3219] * common_minima2), (-959.6407 * np.ones(3))
+    ).all()
+
+
+def test_goldstein_output(common_minima2):
+    """Tests goldstein-price function output."""
+    assert np.isclose(
+        fx.goldstein_func([0, -1] * common_minima2), (3 * np.ones(3))
+    ).all()
+
+
+@pytest.mark.parametrize(
+    "x",
+    [
+        np.array([[3.0, 2.0],
+                  [-2.805118, 3.131312],
+                  [-3.779310, -3.283186],
+                  [3.584428, -1.848126]])
+    ],
+)
+def test_himmelblau_output(x):
+    """Tests himmelblau function output."""
+    assert np.isclose(
+        fx.himmelblau_func(x), np.zeros(4)
+    ).all()
+
+
+@pytest.mark.parametrize(
+    "x",
+    [
+        np.array([[8.05502, 9.66459],
+                  [-8.05502, 9.66459],
+                  [8.05502, -9.66459],
+                  [-8.05502, -9.66459]])
+    ],
+)
+@pytest.mark.parametrize(
+    "minima",
+    [
+        np.array([-19.2085, -19.2085, -19.2085, -19.2085])
+    ],
+)
+def test_holdertable_output(x, minima):
+    """Tests holdertable function output."""
+    assert np.isclose(
+        fx.holdertable_func(x), minima
+    ).all()
 
 
 def test_levi_output(common_minima2):
@@ -70,6 +125,33 @@ def test_levi_output(common_minima2):
     assert np.isclose(fx.levi_func(common_minima2), np.zeros(3)).all()
 
 
+def test_matyas_output(common_minima):
+    """Test matyas function output."""
+    assert np.isclose(fx.matyas_func(common_minima), np.zeros(3)).all()
+
+
+def test_rastrigin_output(common_minima):
+    """Tests rastrigin function output."""
+    assert np.array_equal(fx.rastrigin_func(common_minima), np.zeros(3))
+
+
+def test_rosenbrock_output(common_minima2):
+    """Tests rosenbrock function output."""
+    assert np.array_equal(
+        fx.rosenbrock_func(common_minima2).all(), np.zeros(3).all()
+    )
+
+
 def test_schaffer2_output(common_minima):
     """Test schaffer2 function output."""
     assert np.isclose(fx.schaffer2_func(common_minima), np.zeros(3)).all()
+
+
+def test_sphere_output(common_minima):
+    """Tests sphere function output."""
+    assert np.array_equal(fx.sphere_func(common_minima), np.zeros((3,)))
+
+
+def test_threehump_output(common_minima):
+    """Tests threehump function output."""
+    assert np.array_equal(fx.threehump_func(common_minima), np.zeros(3))
