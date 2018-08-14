918b46e8d169bd355345321a261df27771b8b31d
Lj Miranda <12949683+ljvmiranda921@users.noreply.github.com>
2018-08-14 09:39:18 +0900

    Remove environments module (#217)
    Removed the `environments` module in favor of the `plotters` module.
    
    Signed-off by: ljvmiranda921 <ljvmiranda@gmail.com>

--- pyswarms/utils/environments/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-The mod:`pyswarms.utils.environments` module implements various
-optimization environments to analyze optimizer performance or search
-better parameters
-"""
-
-from .plot_environment import PlotEnvironment
-
-__all__ = ["PlotEnvironment"]
