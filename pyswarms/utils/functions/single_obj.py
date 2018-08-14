eb9ad68b2b3d1b6e8cba80cc62b96d2f3896cf35
Jericho Arcelao <jerichoarcelao@gmail.com>
2018-08-14 17:03:42 +0800

    Replace accidental erasures of '_func' in non single-object functions

--- pyswarms/utils/functions/single_obj.py
+++ pyswarms/utils/functions/single_obj.py
@@ -39,7 +39,7 @@ Function list:
 # Import from __future__
 from __future__ import with_statement
 from __future__ import absolute_import
-from __future__ import printtion
+from __future__ import print_function
 
 # Import modules
 import numpy as np
