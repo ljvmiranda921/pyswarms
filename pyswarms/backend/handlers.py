6e37366b80dcc64ebfbd5ec0173a5e8242a4fbce
ljvmiranda921 <ljvmiranda@gmail.com>
2018-09-08 20:09:39 +0900

    [WIP] Use dict instead of if-else for strats

--- pyswarms/backend/handlers.py
+++ pyswarms/backend/handlers.py
@@ -1,12 +1,12 @@
-"""
-Handler Backend
+"""Handlers
 
-This module provides Handler classes for the position as well as
-the velocity of particles. This is necessary when boundary conditions
-are imposed on the PSO algorithm. Particles that do not stay inside
-these boundary conditions have to be handled by either adjusting their
-position after they left the bounded search space or adjusting their
-velocity when it would position them outside the search space.
+This module provides Handler classes for the position as well as the velocity
+of particles. This is necessary when boundary conditions are imposed on the PSO
+algorithm. Particles that do not stay inside these boundary conditions have to
+be handled by either adjusting their position after they left the bounded
+search space or adjusting their velocity when it would position them outside
+the search space.
+
 """
 
 import logging
@@ -15,8 +15,6 @@ import numpy as np
 
 from ..util.reporter import Reporter
 
-rep = Reporter(logger=logging.getLogger(__name__))
-
 
 class BoundaryHandler(object):
     def __init__(self, strategy):
@@ -24,8 +22,8 @@ class BoundaryHandler(object):
 
         This class offers a way to handle boundary conditions. It contains
         methods to avoid having particles outside of the defined boundaries.
-        It repairs the position of particles that would leave the boundares
-        in the next optimization step by using one of the follwing methods:
+        It repairs the position of particles that would leave the boundares in
+        the next optimization step by using one of the follwing methods:
 
         * Nearest:
             Reposition the particle to the nearest bound.
@@ -39,27 +37,10 @@ class BoundaryHandler(object):
             bounds.
         * Intermediate:
             Reposition the particle to the midpoint between its current
-            position on the bound surpassing axis and the bound itself.
-            This only adjusts the axes that surpass the boundaries.
+            position on the bound surpassing axis and the bound itself.  This
+            only adjusts the axes that surpass the boundaries.
         * Resample:
             Redraw the velocity until the next position is inside the bounds.
-
-        Attributes
-        ----------
-        strategy : str
-            The strategy to be used. The following are available:
-            * "nearest"
-            * "random"
-            * "shrink"
-            * "reflective"
-            * "intermediate"
-            * "resample"
-            For a description of these see above.
-        """
-        self.strategy = strategy
-
-    def __call__(self, position, bounds, *args, **kwargs):
-        """Make class callable
 
         The BoundaryHandler can be called as a function to use the strategy
         that is passed at initialization to repair boundary issues. An example
@@ -73,6 +54,30 @@ class BoundaryHandler(object):
             bh = BoundaryHandler(strategy="reflective")
             ops.compute_position(swarm, bounds, handler=bh)
 
+        Attributes
+        ----------
+        strategy : str
+            The strategy to be used. The following are available:
+            * "nearest"
+            * "random"
+            * "shrink"
+            * "reflective"
+            * "intermediate"
+            * "resample"
+        """
+        self.strategy = strategy
+        self.strats = {
+            "nearest": self.nearest,
+            "random": self.random,
+            "shrink": self.shrink,
+            "reflective": self.reflective,
+            "intermediate": self.intermediate,
+            "resample": self.resample,
+        }
+        self.rep = Reporter(logger=logging.getLogger(__name__))
+
+    def __call__(self, position, bounds, **kwargs):
+        """Apply the selected strategy to the position-matrix
 
         Parameters
         ----------
@@ -82,45 +87,32 @@ class BoundaryHandler(object):
             a tuple of size 2 where the first entry is the minimum bound while
             the second entry is the maximum bound. Each array must be of shape
             :code:`(dimensions,)`
-        *args : tuple
-        **kwargs : dict
+        kwargs : dict
 
         Returns
         -------
         numpy.ndarray
             the adjusted positions of the swarm
         """
-        # Assign new attributes
-        self.position = position
-        self.lower_bound, self.upper_bound = bounds
-        self.__out_of_bounds()
+        kwargs_ = {**{"position": position, "bounds": bounds}, **kwargs}
 
-        if self.strategy == "nearest":
-            new_position = self.nearest()
-        elif self.strategy == "reflective":
-            new_position = self.reflective()
-        elif self.strategy == "shrink":
-            new_position = self.shrink()
-        elif self.strategy == "random":
-            new_position = self.random()
-        elif self.strategy == "intermediate":
-            new_position = self.random()
-        elif self.strategy == "resample":
-            new_position = self.resample()
-
-        return new_position
+        try:
+            new_position = self.strats[self.strategy](**kwargs_)
+        except KeyError:
+            message = "Unrecognized strategy: {}. Choose one among: " + str(
+                [strat for strat in self.strats.keys()]
+            )
+            self.rep.log.exception(message.format(self.strategy))
+            raise
+        else:
+            return new_position
 
     def __out_of_bounds(self):
-        """
-        Helper method to find indices
-
-        This helper methods finds the indices of the positions that do
-        transgress the imposed bounds and stores them in class attributes
-        """
+        """Helper method to find indices of out-of-bound positions"""
         self.greater_than_bound = np.nonzero(self.position > self.upper_bound)
         self.lower_than_bound = np.nonzero(self.position < self.lower_bound)
 
-    def nearest(self):
+    def nearest(self, **kwargs):
         """
         Set position to nearest bound
 
@@ -134,13 +126,13 @@ class BoundaryHandler(object):
         self.position = np.where(bool_greater, self.upper_bound, self.position)
         self.position = np.where(bool_lower, self.lower_bound, self.position)
 
-    def reflective(self):
+    def reflective(self, **kwargs):
         pass
 
-    def shrink(self):
+    def shrink(self, **kwargs):
         pass
 
-    def random(self):
+    def random(self, **kwargs):
         """
         Set position to random location
 
@@ -156,8 +148,8 @@ class BoundaryHandler(object):
             ]
         )
 
-    def intermediate(self):
+    def intermediate(self, **kwargs):
         pass
 
-    def resample(self):
+    def resample(self, **kwargs):
         pass
