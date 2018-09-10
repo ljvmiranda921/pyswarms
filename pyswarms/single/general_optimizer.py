a04fb9a9f59a212f5ff847b4247f6f73df666c45
ljvmiranda921 <ljvmiranda@gmail.com>
2018-09-10 23:46:28 +0900

    [ci skip] Add TODOs

--- pyswarms/single/general_optimizer.py
+++ pyswarms/single/general_optimizer.py
@@ -258,6 +258,7 @@ class GeneralOptimizerPSO(SwarmOptimizer):
             )
             best_cost_yet_found = self.swarm.best_cost
             # If the topology is a ring topology just use the local minimum
+            # TODO
             if isinstance(self.top, Ring) and not isinstance(
                 self.top, VonNeumann
             ):
@@ -266,12 +267,14 @@ class GeneralOptimizerPSO(SwarmOptimizer):
                     self.swarm, self.p, self.k
                 )
             # If the topology is a VonNeumann topology pass the neighbour and range attribute to compute_gbest()
+            # TODO
             if isinstance(self.top, VonNeumann):
                 # Update gbest from neighborhood
                 self.swarm.best_pos, self.swarm.best_cost = self.top.compute_gbest(
                     self.swarm, self.p, self.r
                 )
             # If the topology is a random topology pass the neighbor attribute to compute_gbest()
+            # TODO
             elif isinstance(self.top, Random):
                 # Get minima of pbest and check if it's less than gbest
                 if np.min(self.swarm.pbest_cost) < self.swarm.best_cost:
@@ -280,6 +283,7 @@ class GeneralOptimizerPSO(SwarmOptimizer):
                     )
             else:
                 # Get minima of pbest and check if it's less than gbest
+                # TODO
                 if np.min(self.swarm.pbest_cost) < self.swarm.best_cost:
                     self.swarm.best_pos, self.swarm.best_cost = self.top.compute_gbest(
                         self.swarm
