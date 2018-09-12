08dfeb188883c3c6e876340ce5d3b51cc4cbf2ff
ljvmiranda921 <ljvmiranda@gmail.com>
2018-09-12 15:12:44 +0900

    [ci skip] [WIP] Update Ring topology call

--- tests/backend/topology/test_ring.py
+++ tests/backend/topology/test_ring.py
@@ -14,8 +14,8 @@ from pyswarms.backend.topology import Ring
 @pytest.mark.parametrize("p", [1, 2])
 def test_update_gbest_neighborhood(swarm, p, k, static):
     """Test if update_gbest_neighborhood gives the expected return values"""
-    topology = Ring(static=static)
-    pos, cost = topology.compute_gbest(swarm, p=p, k=k)
+    topology = Ring(static=static, p=p, k=k)
+    pos, cost = topology.compute_gbest(swarm)
     expected_pos = np.array([9.90438476e-01, 2.50379538e-03, 1.87405987e-05])
     expected_cost = 1.0002528364353296
     assert cost == pytest.approx(expected_cost)
@@ -26,7 +26,7 @@ def test_update_gbest_neighborhood(swarm, p, k, static):
 @pytest.mark.parametrize("clamp", [None, (0, 1), (-1, 1)])
 def test_compute_velocity_return_values(swarm, clamp, static):
     """Test if compute_velocity() gives the expected shape and range"""
-    topology = Ring(static=static)
+    topology = Ring(static=static, p=1, k=2)
     v = topology.compute_velocity(swarm, clamp)
     assert v.shape == swarm.position.shape
     if clamp is not None:
@@ -40,7 +40,7 @@ def test_compute_velocity_return_values(swarm, clamp, static):
 )
 def test_compute_position_return_values(swarm, bounds, static):
     """Test if compute_position() gives the expected shape and range"""
-    topology = Ring(static=static)
+    topology = Ring(static=static, p=1, k=2)
     p = topology.compute_position(swarm, bounds)
     assert p.shape == swarm.velocity.shape
     if bounds is not None:
@@ -52,6 +52,6 @@ def test_compute_position_return_values(swarm, bounds, static):
 @pytest.mark.parametrize("p", [1, 2])
 def test_neighbor_idx(swarm, static, p, k):
     """Test if the neighbor_idx attribute is assigned"""
-    topology = Ring(static=static)
-    topology.compute_gbest(swarm, p=p, k=k)
+    topology = Ring(static=static, p=p, k=k)
+    topology.compute_gbest(swarm)
     assert topology.neighbor_idx is not None
