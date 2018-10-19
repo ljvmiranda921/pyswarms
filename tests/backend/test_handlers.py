efbc70192a4eaa0b8d9d5cfd3e04d15eee078109
Aaron Moser <39431903+whzup@users.noreply.github.com>
2018-10-19 21:04:48 +0200

    [WIP] Add assert_bounds and VH test structure
    Added an assert_bounds function to the Handler tests, so we save some
    lines of code.

--- tests/backend/test_handlers.py
+++ tests/backend/test_handlers.py
@@ -21,9 +21,7 @@ def test_out_of_bounds(bounds, positions_inbound, positions_out_of_bound):
     assert idx_inbound.all() == None
     assert idx_out_of_bounds.all() == expected_idx
 
-def test_nearest_strategy(bounds, positions_inbound, positions_out_of_bound):
-    bh = BoundaryHandler(strategy="nearest")
-
+def assert_bounds(bounds, positions_inbound, positions_out_of_bound, bh):
     # Test if it doesn't handle inbound positions
     inbound_handled = bh(positions_inbound, bounds)
     assert inbound_handled.all() == positions_inbound.all()
@@ -35,92 +33,51 @@ def test_nearest_strategy(bounds, positions_inbound, positions_out_of_bound):
     assert not lower_than_bound.all()
     assert not greater_than_bound.all()
 
+def assert_clamp():
+    pass
 
+def test_nearest_strategy(bounds, positions_inbound, positions_out_of_bound):
+    bh = BoundaryHandler(strategy="nearest")
+    assert_bounds(bounds, positions_inbound, positions_out_of_bound, bh)
     # TODO Add strategy specific tests
 
 def test_reflective_strategy(bounds, positions_inbound, positions_out_of_bound):
     bh = BoundaryHandler(strategy="reflective")
-
-    # Test if it doesn't handle inbound positions
-    # inbound_handled = bh(positions_inbound, bounds)
-    # assert inbound_handled.all() == positions_inbound.all()
-
-    # Test if all particles are handled to a position inside the boundaries
-    # outbound_handled = bh(positions_out_of_bound, bounds)
-    # lower_than_bound = outbound_handled < bounds[0]
-    # greater_than_bound = outbound_handled > bounds[1]
-    # assert not lower_than_bound.all()
-    # assert not greater_than_bound.all()
     pass
-
-
     # TODO Add strategy specific tests
 
 def test_shrink_strategy(bounds, positions_inbound, positions_out_of_bound):
     bh = BoundaryHandler(strategy="shrink")
-
-    # Test if it doesn't handle inbound positions
-    inbound_handled = bh(positions_inbound, bounds)
-    assert inbound_handled.all() == positions_inbound.all()
-
-    # Test if all particles are handled to a position inside the boundaries
-    outbound_handled = bh(positions_out_of_bound, bounds)
-    lower_than_bound = outbound_handled < bounds[0]
-    greater_than_bound = outbound_handled > bounds[1]
-    assert not lower_than_bound.all()
-    assert not greater_than_bound.all()
-
-
+    assert_bounds(bounds, positions_inbound, positions_out_of_bound, bh)
     # TODO Add strategy specific tests
 
 def test_random_strategy(bounds, positions_inbound, positions_out_of_bound):
     bh = BoundaryHandler(strategy="random")
-
-    # Test if it doesn't handle inbound positions
-    inbound_handled = bh(positions_inbound, bounds)
-    assert inbound_handled.all() == positions_inbound.all()
-
-    # Test if all particles are handled to a position inside the boundaries
-    outbound_handled = bh(positions_out_of_bound, bounds)
-    lower_than_bound = outbound_handled < bounds[0]
-    greater_than_bound = outbound_handled > bounds[1]
-    assert not lower_than_bound.all()
-    assert not greater_than_bound.all()
-
-
+    assert_bounds(bounds, positions_inbound, positions_out_of_bound, bh)
     # TODO Add strategy specific tests
 
 def test_intermediate_strategy(bounds, positions_inbound, positions_out_of_bound):
     bh = BoundaryHandler(strategy="intermediate")
-
-    # Test if it doesn't handle inbound positions
-    inbound_handled = bh(positions_inbound, bounds)
-    assert inbound_handled.all() == positions_inbound.all()
-
-    # Test if all particles are handled to a position inside the boundaries
-    outbound_handled = bh(positions_out_of_bound, bounds)
-    lower_than_bound = outbound_handled < bounds[0]
-    greater_than_bound = outbound_handled > bounds[1]
-    assert not lower_than_bound.all()
-    assert not greater_than_bound.all()
-
-
+    assert_bounds(bounds, positions_inbound, positions_out_of_bound, bh)
     # TODO Add strategy specific tests
 
 def test_periodic_strategy(bounds, positions_inbound, positions_out_of_bound):
     bh = BoundaryHandler(strategy="periodic")
-
-    # Test if it doesn't handle inbound positions
-    inbound_handled = bh(positions_inbound, bounds)
-    assert inbound_handled.all() == positions_inbound.all()
-
-    # Test if all particles are handled to a position inside the boundaries
-    outbound_handled = bh(positions_out_of_bound, bounds)
-    lower_than_bound = outbound_handled < bounds[0]
-    greater_than_bound = outbound_handled > bounds[1]
-    assert not lower_than_bound.all()
-    assert not greater_than_bound.all()
-
-
+    assert_bounds(bounds, positions_inbound, positions_out_of_bound, bh)
     # TODO Add strategy specific tests
 
+def test_unmodified_strategy():
+    vh = VelocityHandler(strategy="unmodified")
+    pass
+
+def test_adjust_strategy():
+    vh = VelocityHandler(strategy="adjust")
+    pass
+
+def test_invert_strategy():
+    vh = VelocityHandler(strategy="invert")
+    pass
+
+def test_zero_strategy():
+    vh = VelocityHandler(strategy="zero")
+    pass
