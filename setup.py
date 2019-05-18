d618bd945d35fedd1b06a4e2ef3f12d938ef1e36
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-05-18 16:40:05 +0800

    Bump version: 1.0.2 → 1.1.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("requirements-dev.txt") as f:
 
 setup(
     name="pyswarms",
-    version="1.0.2",
+    version="1.1.0",
     description="A Python-based Particle Swarm Optimization (PSO) library.",
     long_description=readme,
     long_description_content_type="text/markdown",
