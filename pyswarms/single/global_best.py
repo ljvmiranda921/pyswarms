c5ad4e10f84327a15155e3a03e601b3a46fd95e2
msat59 <mehdi.sattari@gmail.com>
2020-05-05 22:29:19 +0200

    Fix issue #397
    Fixed the unwanted early stopping if best_cost doesn't changed in two consecutive iteration.
    In addition, the default value of ftol_iter was changed to 0 (disabled), and its description updated.

--- pyswarms/single/global_best.py
+++ pyswarms/single/global_best.py
@@ -196,7 +196,6 @@ class GlobalBestPSO(SwarmOptimizer):
         ftol_history = [None] * self.ftol_iter
         # Default reason for optimization completion
         stop_reason = "«iters={}» reached".format(iters)
-        
         for i in range(iters) if verbose else self.rep.pbar(iters, self.name):
             # Compute cost for current position and personal best
             # fmt: off
