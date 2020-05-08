cfa39ab1d6fdc796e7589f00ae444829391cc796
msat59 <mehdi.sattari@gmail.com>
2020-05-09 01:01:28 +0200

    Fix for broken progress bar

--- pyswarms/single/local_best.py
+++ pyswarms/single/local_best.py
@@ -220,7 +220,7 @@ class LocalBestPSO(SwarmOptimizer):
         ftol_history = [None] * self.ftol_iter
         # Default reason for optimization completion
         stop_reason = "«iters={}» reached".format(iters)
-        for i in range(iters) if verbose else self.rep.pbar(iters, self.name):
+        for i in self.rep.pbar(iters, self.name) if verbose else range(iters):
             # Compute cost for current position and personal best
             self.swarm.current_cost = compute_objective_function(
                 self.swarm, objective_func, pool=pool, **kwargs
