9182b945b9a7d07c8f4998c49f2bb0a2d960ac7a
Matthijs Wesseling <matthijswesseling@gmail.com>
2023-09-19 16:25:16 +0200

    Type hint generators

--- pyswarms/backend/generators.py
+++ pyswarms/backend/generators.py
@@ -11,9 +11,11 @@ here to dictate how a swarm is initialized for your custom PSO.
 
 # Import standard library
 import logging
+from typing import Any, Dict, List, Optional, Tuple
 
 # Import modules
 import numpy as np
+import numpy.typing as npt
 
 from ..utils.reporter import Reporter
 from .swarms import Swarm
@@ -21,7 +23,13 @@ from .swarms import Swarm
 rep = Reporter(logger=logging.getLogger(__name__))
 
 
-def generate_swarm(n_particles, dimensions, bounds=None, center=1.00, init_pos=None):
+def generate_swarm(
+    n_particles: int, 
+    dimensions: int, 
+    bounds: Optional[Tuple[List[int], List[int]]] = None, 
+    center: float|npt.NDArray[np.floating[Any]] = 1.00, 
+    init_pos: Optional[npt.NDArray[np.floating[Any]]] = None,
+) -> npt.NDArray[np.floating[Any]]:
     """Generate a swarm
 
     Parameters
@@ -55,33 +63,31 @@ def generate_swarm(n_particles, dimensions, bounds=None, center=1.00, init_pos=N
     TypeError
         When the argument passed to bounds is not an iterable.
     """
-    try:
-        if (init_pos is not None) and (bounds is None):
-            pos = init_pos
-        elif (init_pos is not None) and (bounds is not None):
+    if bounds is not None:
+        if init_pos is not None:
             if not (np.all(bounds[0] <= init_pos) and np.all(init_pos <= bounds[1])):
                 raise ValueError("User-defined init_pos is out of bounds.")
             pos = init_pos
-        elif (init_pos is None) and (bounds is None):
-            pos = center * np.random.uniform(low=0.0, high=1.0, size=(n_particles, dimensions))
         else:
-            lb, ub = bounds
-            min_bounds = np.repeat(np.array(lb)[np.newaxis, :], n_particles, axis=0)
-            max_bounds = np.repeat(np.array(ub)[np.newaxis, :], n_particles, axis=0)
-            pos = center * np.random.uniform(low=min_bounds, high=max_bounds, size=(n_particles, dimensions))
-    except ValueError:
-        msg = "Bounds and/or init_pos should be of size ({},)"
-        rep.logger.exception(msg.format(dimensions))
-        raise
-    except TypeError:
-        msg = "generate_swarm() takes an int for n_particles and dimensions and an array for bounds"
-        rep.logger.exception(msg)
-        raise
+            try:
+                lb, ub = bounds
+                min_bounds = np.repeat(np.array(lb)[np.newaxis, :], n_particles, axis=0)
+                max_bounds = np.repeat(np.array(ub)[np.newaxis, :], n_particles, axis=0)
+                pos = center * np.random.uniform(low=min_bounds, high=max_bounds, size=(n_particles, dimensions))
+            except ValueError as e:
+                msg = "Bounds and/or init_pos should be of size ({},)"
+                rep.logger.exception(msg.format(dimensions))
+                raise e
     else:
-        return pos
+        if init_pos is not None:
+            pos = init_pos
+        else:
+            pos = center * np.random.uniform(low=0.0, high=1.0, size=(n_particles, dimensions))
+
+    return pos
 
 
-def generate_discrete_swarm(n_particles, dimensions, binary=False, init_pos=None):
+def generate_discrete_swarm(n_particles: int, dimensions: int, binary: bool = False, init_pos: Optional[npt.NDArray[np.integer[Any]]] = None) -> npt.NDArray[np.integer[Any]]:
     """Generate a discrete swarm
 
     Parameters
@@ -109,30 +115,19 @@ def generate_discrete_swarm(n_particles, dimensions, binary=False, init_pos=None
     TypeError
         When the argument passed to n_particles or dimensions is incorrect.
     """
-    try:
-        if (init_pos is not None) and binary:
-            if not len(np.unique(init_pos)) <= 2:
-                raise ValueError("User-defined init_pos is not binary!")
-            # init_pos maybe ones
-            pos = init_pos
-        elif (init_pos is not None) and not binary:
-            pos = init_pos
-        elif (init_pos is None) and binary:
+    if init_pos is not None:
+        if binary and len(np.unique(init_pos)) > 2:
+            raise ValueError("User-defined init_pos is not binary!")
+        pos = init_pos
+    else:
+        if binary:
             pos = np.random.randint(2, size=(n_particles, dimensions))
         else:
             pos = np.random.random_sample(size=(n_particles, dimensions)).argsort(axis=1)
-    except ValueError:
-        rep.logger.exception("Please check the size and value of dimensions")
-        raise
-    except TypeError:
-        msg = "generate_discrete_swarm() takes an int for n_particles and dimensions"
-        rep.logger.exception(msg)
-        raise
-    else:
-        return pos
+    return pos
 
 
-def generate_velocity(n_particles, dimensions, clamp=None):
+def generate_velocity(n_particles: int, dimensions: int, clamp: Optional[Tuple[float, float]] = None) -> npt.NDArray[Any]:
     """Initialize a velocity vector
 
     Parameters
@@ -164,64 +159,5 @@ def generate_velocity(n_particles, dimensions, clamp=None):
         msg = "generate_velocity() takes an int for n_particles and dimensions and an array for clamp"
         rep.logger.exception(msg)
         raise
-    else:
-        return velocity
 
-
-def create_swarm(
-    n_particles,
-    dimensions,
-    discrete=False,
-    binary=False,
-    options={},
-    bounds=None,
-    center=1.0,
-    init_pos=None,
-    clamp=None,
-):
-    """Abstract the generate_swarm() and generate_velocity() methods
-
-    Parameters
-    ----------
-    n_particles : int
-        number of particles to be generated in the swarm.
-    dimensions: int
-        number of dimensions to be generated in the swarm
-    discrete : bool
-        Creates a discrete swarm. Default is `False`
-    options : dict, optional
-        Swarm options, for example, c1, c2, etc.
-    binary : bool
-        generate a binary matrix, Default is `False`
-    bounds : tuple of np.ndarray or list
-        a tuple of size 2 where the first entry is the minimum bound while
-        the second entry is the maximum bound. Each array must be of shape
-        :code:`(dimensions,)`. Default is `None`
-    center : numpy.ndarray, optional
-        a list of initial positions for generating the swarm. Default is `1`
-    init_pos : numpy.ndarray, optional
-        option to explicitly set the particles' initial positions. Set to
-        :code:`None` if you wish to generate the particles randomly.
-    clamp : tuple of floats, optional
-        a tuple of size 2 where the first entry is the minimum velocity
-        and the second entry is the maximum velocity. It
-        sets the limits for velocity clamping.
-
-    Returns
-    -------
-    pyswarms.backend.swarms.Swarm
-        a Swarm class
-    """
-    if discrete:
-        position = generate_discrete_swarm(n_particles, dimensions, binary=binary, init_pos=init_pos)
-    else:
-        position = generate_swarm(
-            n_particles,
-            dimensions,
-            bounds=bounds,
-            center=center,
-            init_pos=init_pos,
-        )
-
-    velocity = generate_velocity(n_particles, dimensions, clamp=clamp)
-    return Swarm(position, velocity, options=options)
+    return velocity
