5343e988d0ca7b965ba73aaaf90726a6a3103941
Matthijs Wesseling <matthijswesseling@gmail.com>
2023-09-22 10:00:07 +0200

    Fix init

--- pyswarms/backend/__init__.py
+++ pyswarms/backend/__init__.py
@@ -4,7 +4,7 @@ for swarm optimization: generating boundaries, updating positions, etc.
 You can use the methods implemented here to build your own PSO implementations.
 """
 
-from .generators import create_swarm, generate_discrete_swarm, generate_swarm, generate_velocity
-from .handlers import BoundaryHandler, OptionsHandler, VelocityHandler
-from .operators import compute_objective_function, compute_pbest, compute_position, compute_velocity
-from .swarms import Swarm
+from pyswarms.backend.generators import generate_discrete_swarm, generate_swarm, generate_velocity
+from pyswarms.backend.handlers import BoundaryHandler, OptionsHandler, VelocityHandler
+from pyswarms.backend.operators import compute_objective_function, compute_pbest, compute_position, compute_velocity
+from pyswarms.backend.swarms import Swarm
