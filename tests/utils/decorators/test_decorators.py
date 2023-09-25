6f2a07109c5deb1a6722b68a8f0b40e30679559c
Matthijs Wesseling <matthijswesseling@gmail.com>
2023-09-25 13:07:37 +0200

    Fix functions and decorators

--- tests/utils/decorators/test_decorators.py
+++ tests/utils/decorators/test_decorators.py
@@ -1,24 +1,24 @@
 # Import modules
+from typing import Any, Callable
 import numpy as np
+import numpy.typing as npt
 import pytest
 
-# Import from pyswarms
-# Import from package
 from pyswarms.utils.decorators import cost
 
 
 @pytest.mark.parametrize("objective_func", [np.sum, np.prod])
-def test_cost_decorator(objective_func, particles):
+def test_cost_decorator(objective_func: Callable[[npt.NDArray[Any]], float], particles: npt.NDArray[Any]):
     """Test if cost decorator returns the same shape and value as undecorated function"""
     n_particles = particles.shape[0]
 
-    def cost_func_without_decorator(x):
+    def cost_func_without_decorator(x: npt.NDArray[Any]):
         n_particles_in_func = x.shape[0]
         cost = np.array([objective_func(x[i]) for i in range(n_particles_in_func)])
         return cost
 
     @cost
-    def cost_func_with_decorator(x):
+    def cost_func_with_decorator(x: npt.NDArray[Any]):
         cost = objective_func(x)
         return cost
 
@@ -29,15 +29,15 @@ def test_cost_decorator(objective_func, particles):
     assert decorated.shape == (n_particles,)
 
 
-def test_decorator_invalid_cost_func(particles):
+def test_decorator_invalid_cost_func(particles: npt.NDArray[Any]):
     """Test if ValueError is raised whenever an invalid cost function is passed"""
 
-    def objective_func(x):
+    def objective_func(x: npt.NDArray[Any]):
         """Returns a numpy.ndarray instead of int or float"""
         return np.array([1, 3])
 
-    @cost
-    def cost_func_with_wrong_output_shape_decorated(x):
+    @cost # type: ignore
+    def cost_func_with_wrong_output_shape_decorated(x: npt.NDArray[Any]):
         cost = objective_func(x)
         return cost
 
--- tests/utils/functions/test_singleobj_return.py
+++ tests/utils/functions/test_singleobj_return.py
@@ -3,57 +3,53 @@
 
 # Import modules
 import numpy as np
-import pytest
 
 # Import from pyswarms
 from pyswarms.utils.functions import single_obj as fx
 
 
-def test_ackley_output(common_minima):
+common_minima = np.zeros(shape=(3, 2))
+common_minima2 = np.ones(shape=(3, 2))
+
+
+def test_ackley_output():
     """Tests ackley function output."""
     assert np.isclose(fx.ackley(common_minima), np.zeros(3)).all()
 
 
-def test_beale_output(common_minima2):
+def test_beale_output():
     """Tests beale function output."""
     assert np.isclose(fx.beale([3, 0.5] * common_minima2), np.zeros(3)).all()
 
 
-def test_booth_output(common_minima2):
+def test_booth_output():
     """Test booth function output."""
     assert np.isclose(fx.booth([1, 3] * common_minima2), np.zeros(3)).all()
 
 
-def test_bukin6_output(common_minima2):
+def test_bukin6_output():
     """Test bukin function output."""
     assert np.isclose(fx.bukin6([-10, 1] * common_minima2), np.zeros(3)).all()
 
 
-@pytest.mark.parametrize(
-    "x",
-    [
-        np.array(
-            [
-                [1.34941, -1.34941],
-                [1.34941, 1.34941],
-                [-1.34941, 1.34941],
-                [-1.34941, -1.34941],
-            ]
-        )
-    ],
-)
-@pytest.mark.parametrize("minima", [np.array([-2.06261, -2.06261, -2.06261, -2.06261])])
-def test_crossintray_output(x, minima):
+def test_crossintray_output():
     """Tests crossintray function output."""
+    x = np.array([
+        [1.34941, -1.34941],
+        [1.34941, 1.34941],
+        [-1.34941, 1.34941],
+        [-1.34941, -1.34941],
+    ])
+    minima = np.array([-2.06261, -2.06261, -2.06261, -2.06261])
     assert np.isclose(fx.crossintray(x), minima).all()
 
 
-def test_easom_output(common_minima2):
+def test_easom_output():
     """Tests easom function output."""
     assert np.isclose(fx.easom([np.pi, np.pi] * common_minima2), (-1 * np.ones(3))).all()
 
 
-def test_eggholder_output(common_minima2):
+def test_eggholder_output():
     """Tests eggholder function output."""
     assert np.isclose(
         fx.eggholder([512, 404.3219] * common_minima2),
@@ -61,33 +57,26 @@ def test_eggholder_output(common_minima2):
     ).all()
 
 
-def test_goldstein_output(common_minima2):
+def test_goldstein_output():
     """Tests goldstein-price function output."""
     assert np.isclose(fx.goldstein([0, -1] * common_minima2), (3 * np.ones(3))).all()
 
 
-@pytest.mark.parametrize(
-    "x",
-    [
-        np.array(
-            [
-                [3.0, 2.0],
-                [-2.805118, 3.131312],
-                [-3.779310, -3.283186],
-                [3.584428, -1.848126],
-            ]
-        )
-    ],
-)
-def test_himmelblau_output(x):
+
+def test_himmelblau_output():
     """Tests himmelblau function output."""
+    x = np.array([
+        [3.0, 2.0],
+        [-2.805118, 3.131312],
+        [-3.779310, -3.283186],
+        [3.584428, -1.848126],
+    ])
     assert np.isclose(fx.himmelblau(x), np.zeros(4)).all()
 
 
-@pytest.mark.parametrize(
-    "x",
-    [
-        np.array(
+def test_holdertable_output():
+    """Tests holdertable function output."""
+    x = np.array(
             [
                 [8.05502, 9.66459],
                 [-8.05502, 9.66459],
@@ -95,44 +84,40 @@ def test_himmelblau_output(x):
                 [-8.05502, -9.66459],
             ]
         )
-    ],
-)
-@pytest.mark.parametrize("minima", [np.array([-19.2085, -19.2085, -19.2085, -19.2085])])
-def test_holdertable_output(x, minima):
-    """Tests holdertable function output."""
+    minima = np.array([-19.2085, -19.2085, -19.2085, -19.2085])
     assert np.isclose(fx.holdertable(x), minima).all()
 
 
-def test_levi_output(common_minima2):
+def test_levi_output():
     """Test levi function output."""
     assert np.isclose(fx.levi(common_minima2), np.zeros(3)).all()
 
 
-def test_matyas_output(common_minima):
+def test_matyas_output():
     """Test matyas function output."""
     assert np.isclose(fx.matyas(common_minima), np.zeros(3)).all()
 
 
-def test_rastrigin_output(common_minima):
+def test_rastrigin_output():
     """Tests rastrigin function output."""
     assert np.array_equal(fx.rastrigin(common_minima), np.zeros(3))
 
 
-def test_rosenbrock_output(common_minima2):
+def test_rosenbrock_output():
     """Tests rosenbrock function output."""
     assert np.array_equal(fx.rosenbrock(common_minima2).all(), np.zeros(3).all())
 
 
-def test_schaffer2_output(common_minima):
+def test_schaffer2_output():
     """Test schaffer2 function output."""
     assert np.isclose(fx.schaffer2(common_minima), np.zeros(3)).all()
 
 
-def test_sphere_output(common_minima):
+def test_sphere_output():
     """Tests sphere function output."""
     assert np.array_equal(fx.sphere(common_minima), np.zeros((3,)))
 
 
-def test_threehump_output(common_minima):
+def test_threehump_output():
     """Tests threehump function output."""
     assert np.array_equal(fx.threehump(common_minima), np.zeros(3))
