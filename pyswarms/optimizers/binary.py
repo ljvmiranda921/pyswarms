d09be5023b438b43cff184c0f0e7501e7434f78c
Matthijs Wesseling <matthijswesseling@gmail.com>
2023-09-28 16:19:27 +0200

    Update binary class

--- pyswarms/optimizers/binary.py
+++ pyswarms/optimizers/binary.py
@@ -51,21 +51,16 @@ R.C. Eberhart in Particle Swarm Optimization [SMC1997]_.
     Conference on Systems, Man, and Cybernetics, 1997.
 """
 
-import multiprocessing as mp
-from collections import deque
-from typing import Any, Callable, Deque, Literal, Optional, Tuple
+from typing import Any, Literal, Optional
 
 import numpy as np
 import numpy.typing as npt
-from loguru import logger
-from tqdm import trange
 
-from pyswarms.backend.operators import compute_objective_function, compute_pbest
 from pyswarms.backend.position import PositionUpdater
 from pyswarms.backend.swarms import Swarm
 from pyswarms.backend.topology import Ring
 from pyswarms.backend.velocity import VelocityUpdater
-from pyswarms.optimizers.base import BaseSwarmOptimizer, ToHistory
+from pyswarms.optimizers.base import BaseSwarmOptimizer
 from pyswarms.utils.types import Position
 
 
@@ -120,127 +115,33 @@ class BinaryPSO(BaseSwarmOptimizer):
 
         # Initialize parent class
         super().__init__(
-            n_particles=n_particles,
-            dimensions=dimensions,
-            velocity_updater=velocity_updater,
-            position_updater=position_updater,
-            init_pos=init_pos,
-            ftol=ftol,
-            ftol_iter=ftol_iter,
+            n_particles,
+            dimensions,
+            Ring(self.p, self.k, static=False),
+            velocity_updater,
+            position_updater,
+            init_pos,
+            ftol,
+            ftol_iter,
         )
-        # Initialize the resettable attributes
-        self.reset()
-        # Initialize the topology
-        self.top = Ring(self.p, self.k, static=False)
+
         self.name = __name__
 
-    def optimize(
-        self,
-        objective_func: Callable[..., npt.NDArray[Any]],
-        iters: int,
-        n_processes: Optional[int] = None,
-        verbose: bool = True,
-        **kwargs: Any
-    ) -> Tuple[float, Position]:
-        """Optimize the swarm for a number of iterations
-
-        Performs the optimization to evaluate the objective
-        function :code:`f` for a number of iterations :code:`iter.`
-
-        Parameters
-        ----------
-        objective_func : function
-            objective function to be evaluated
-        iters : int
-            number of iterations
-        n_processes : int, optional
-            number of processes to use for parallel particle evaluation
-            Defaut is None with no parallelization.
-        verbose : bool
-            enable or disable the logs and progress bar (default: True = enable logs)
-        kwargs : dict
-            arguments for objective function
-
-        Returns
-        -------
-        tuple
-            the local best cost and the local best position among the
-            swarm.
-        """
-        log_level = "DEBUG" if verbose else "TRACE"
-        logger.debug("Obj. func. args: {}".format(kwargs))
-
-        # Setup Pool of processes for parallel evaluation
-        pool = None if n_processes is None else mp.Pool(n_processes)
-
-        self.swarm.pbest_cost = np.full(self.swarm_size[0], np.inf)
-        ftol_history: Deque[bool] = deque(maxlen=self.ftol_iter)
-
-        pbar = trange(iters, desc=self.name) if verbose else range(iters)
-        for i in pbar:
-            # Compute cost for current position and personal best
-            self.swarm.current_cost = compute_objective_function(self.swarm, objective_func, pool, **kwargs)
-            self.swarm.pbest_pos, self.swarm.pbest_cost = compute_pbest(self.swarm)
-            best_cost_yet_found = np.min(self.swarm.best_cost)
-
-            # Update gbest from neighborhood
-            self.swarm.best_pos, self.swarm.best_cost = self.top.compute_gbest(self.swarm)
-
-            # Print to console
-            if verbose:
-                pbar.set_postfix(best_cost=self.swarm.best_cost)  # type: ignore
-
-            # Save to history
-            hist = ToHistory(
-                best_cost=self.swarm.best_cost,
-                mean_pbest_cost=float(np.mean(self.swarm.pbest_cost)),
-                mean_neighbor_cost=float(np.mean(self.swarm.best_cost)),
-                position=self.swarm.position,
-                velocity=self.swarm.velocity,
-            )
-            self._populate_history(hist)
-
-            # Verify stop criteria based on the relative acceptable cost ftol
-            relative_measure = self.ftol * (1 + np.abs(best_cost_yet_found))
-            delta = np.abs(self.swarm.best_cost - best_cost_yet_found) < relative_measure
-            if i < self.ftol_iter:
-                ftol_history.append(delta)
-            else:
-                ftol_history.append(delta)
-                if all(ftol_history):
-                    break
-
-            # Perform position velocity update
-            self.swarm.velocity = self.velocity_updater.compute(self.swarm, i, iters)
-            self.swarm.position = self._compute_position(self.swarm)
-
-        # Obtain the final best_cost and the final best_position
-        final_best_cost = self.swarm.best_cost
-        final_best_pos = self.swarm.pbest_pos[self.swarm.pbest_cost.argmin()].copy()
-        logger.log(
-            log_level,
-            "Optimization finished | best cost: {}, best pos: {}".format(final_best_cost, final_best_pos),
+    def _init_swarm(self):
+        position = self.position_updater.generate_discrete_position(
+            self.n_particles, self.dimensions, self.binary, self.init_pos
         )
+        velocity = self.velocity_updater.generate_velocity(self.n_particles, self.dimensions)
+        self.swarm = Swarm(position, velocity)
 
-        # Close Pool of Processes
-        if n_processes is not None:
-            pool.close()  # type: ignore
-
-        return (final_best_cost, final_best_pos)
-
-    def _compute_position(self, swarm: Swarm):
+    def _compute_position(self):
         """Update the position matrix of the swarm
 
         This computes the next position in a binary swarm. It compares the
         sigmoid output of the velocity-matrix and compares it with a randomly
         generated matrix.
-
-        Parameters
-        ----------
-        swarm: pyswarms.backend.swarms.Swarm
-            a Swarm class
         """
-        return (np.random.random_sample(size=swarm.dimensions) < self._sigmoid(swarm.velocity)) * 1
+        return (np.random.random_sample(size=self.swarm.dimensions) < self._sigmoid(self.swarm.velocity)) * 1
 
     def _sigmoid(self, x: npt.NDArray[Any]):
         """Helper method for the sigmoid function
@@ -256,10 +157,3 @@ class BinaryPSO(BaseSwarmOptimizer):
             Output sigmoid computation
         """
         return 1 / (1 + np.exp(-x))
-
-    def _init_swarm(self):
-        position = self.position_updater.generate_discrete_position(
-            self.n_particles, self.dimensions, self.binary, self.init_pos
-        )
-        velocity = self.velocity_updater.generate_velocity(self.n_particles, self.dimensions)
-        self.swarm = Swarm(position, velocity)
